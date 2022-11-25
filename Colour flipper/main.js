01d65153512ce43ecec9549c86a737cfcde0e26c
Marvelous Ejiogu <ejiogumarvelous@gmail.com>
2022-11-25 20:06:07 +0100

    Javascript DOM projects

---
+++ Colour flipper/main.js
@@ -0,0 +1,17 @@
+const colors  = ["Red", "#f15025", "Green", "rdgba(133,122,200)", "cornflowerblue", "#fff", "#00AEB9", "#CBEDEE", "#F7AD19", "#429EBD", "#C17F8B", "#F73668"];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector("color");
+
+btn.addEventListener('click', function() {
+    
+})
+
+// let num = 08131313490
+
+// let numm = num.toString()
+
+// let result = numm.startsWith(0813)
+
+// console.log(result)
